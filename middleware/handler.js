0c6de7e9f95eb7357a120fdcd6db001d4bedde1c
vladimir_sh <vladimir.shkarupin@cprime.com>
2023-10-26 19:25:13 +0300

    Fixed CORS issue

--- middleware/handler.js
+++ middleware/handler.js
@@ -1,11 +1,13 @@
 class RouteHandler {
     setHeaders(req, res, next) {
         // const origin = req.get('origin');
-        res.header('Access-Control-Allow-Origin', "*");
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', '*');
         res.header('Access-Control-Allow-Credentials', 'true');
         res.header(
             'Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH'
         );
+        res.header('Access-Control-Max-Age', 2592000);
         next();
     }
 }
